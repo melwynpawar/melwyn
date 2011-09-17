c963dcd2134d08b5ed389fb1750d13ef142dca4d
Melwyn Pawar <melwynpawar@gmail.com>
2011-09-16 17:55:39 -0700

    done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Melwyn::Application.config.secret_token = '28df37c6a06b2b1c89c48b7c1fa4cf8c31ce3bc9f02a0bf88350e9b601904b8ba0548775d187c3cd5d623be58e2ab1b0559c5235b2b359084c25e4c994af7ef6'
